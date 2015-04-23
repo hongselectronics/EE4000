391691876394461d8e38e5be685d3e1073424601
Jeremy Hong <jkshong@gmail.com>
2015-04-22 23:36:14 -0400

    Create Final_Exam.m

---
+++ Final_Exam.m
@@ -0,0 +1,9 @@
+% Jeremy Hong
+% 4/22/2015
+% Wright State University
+% ©2015 Hong's Electronics
+% ©2015 Sierra Nevada Corporation
+% Copyright notice available at:
+% https://github.com/hongselectronics/EE4000/blob/master/LICENSE.md
+% EE4000-01
+% Spring 2015
